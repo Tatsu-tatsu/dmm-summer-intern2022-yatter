9b0cf03344f9e58ad7a2225ae463ac9f16c25ce0
Tatsu-tatsu <tatsu.nakagawa7@gmail.com>
2022-08-03 15:00:22 +0900

    create GetAccouts

---
+++ app/handler/accounts/get.go
@@ -0,0 +1,30 @@
+package accounts
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"yatter-backend-go/app/handler/httperror"
+
+	"github.com/go-chi/chi"
+)
+
+// Handle request for `POST /v1/accounts`
+func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	username := chi.URLParam(r, "username")
+	a := h.app.Dao.Account() // domain/repository の取得
+	account, err := a.FindByUsername(ctx, username)
+	if err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(account); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+
+}
